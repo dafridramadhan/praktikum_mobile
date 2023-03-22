8185ca964cb4b1695d717f890e03d8f5a527531e
unknown <dafridramadhan@gmail.com>
2023-03-22 22:57:53 +0700

    Minor Update

--- Minggu 4/praktikum_4/lib/percobaan_1/done_culinary_list.dart
+++ Minggu 4/praktikum_4/lib/percobaan_1/done_culinary_list.dart
@@ -12,78 +12,78 @@ class DoneCulinaryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: const Text('Kuliner Telah Dikunjungi'),
-    //   ),
-    //   body: ListView.builder(
-    //       itemBuilder: (context, index) {
-    //         final Culinary food = doneCulinaryList[index];
-    //         return Card(
-    //           color: Colors.white60,
-    //           child: Row(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //             children: <Widget> [
-    //               Expanded(
-    //                 child: Image.asset(
-    //                   food.imageAsset,
-    //                   width: 100,
-    //                   height: 80,
-    //                   fit: BoxFit.cover,
-    //                 ),
-    //               ),
-    //               Expanded(
-    //                 flex: 2,
-    //                 child: Padding(
-    //                   padding: const EdgeInsets.all(8.0),
-    //                   child: Column(
-    //                     crossAxisAlignment: CrossAxisAlignment.start,
-    //                     mainAxisSize: MainAxisSize.min,
-    //                     children: <Widget>[
-    //                       Text(
-    //                         food.name,
-    //                         style: TextStyle(fontSize: 16.0),
-    //                       ),
-    //                       SizedBox(
-    //                         height: 10,
-    //                       ),
-    //                     ],
-    //                   ),
-    //                 ),
-    //               ),
-    //               const Icon(Icons.done_outline)
-    //             ],
-    //           ),
-    //         );
-    //       },
-    //       itemCount: doneCulinaryList.length,
-    //   ),
-    // );
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Kuliner yang telah dikunjungi'),
+        title: const Text('Kuliner Telah Dikunjungi'),
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Culinary food = doneCulinaryList[index];
-          return Card(
-            color: Colors.white60,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  food.name,
-                  style: const TextStyle(fontSize: 16.0, fontFamily: 'Poppins'),
-                ),
-                const Icon(Icons.done_outline),
-              ],
-            ),
-          );
-        },
-        itemCount: doneCulinaryList.length,
+          itemBuilder: (context, index) {
+            final Culinary food = doneCulinaryList[index];
+            return Card(
+              color: Colors.white60,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget> [
+                  Expanded(
+                    child: Image.asset(
+                      food.imageAsset,
+                      width: 100,
+                      height: 80,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            food.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Icon(Icons.done_outline)
+                ],
+              ),
+            );
+          },
+          itemCount: doneCulinaryList.length,
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Kuliner yang telah dikunjungi'),
+    //   ),
+    //   body: ListView.builder(
+    //     itemBuilder: (context, index) {
+    //       final Culinary food = doneCulinaryList[index];
+    //       return Card(
+    //         color: Colors.white60,
+    //         child: Row(
+    //           crossAxisAlignment: CrossAxisAlignment.start,
+    //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //           children: <Widget>[
+    //             Text(
+    //               food.name,
+    //               style: const TextStyle(fontSize: 16.0, fontFamily: 'Poppins'),
+    //             ),
+    //             const Icon(Icons.done_outline),
+    //           ],
+    //         ),
+    //       );
+    //     },
+    //     itemCount: doneCulinaryList.length,
+    //   ),
+    // );
   }
 }
